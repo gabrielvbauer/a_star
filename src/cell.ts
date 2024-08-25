714481ea090a4608a109500bcf9f91586f218c8b
gabriel.bauer <gabriel.bauer@gft.com>
2024-08-25 20:28:03 -0300

    feat: origin and destination

--- src/cell.ts
+++ src/cell.ts
@@ -1,11 +1,14 @@
 import { Graphics, Text } from "pixi.js"
 
+type CellType = 'default' | 'origin' | 'destination'
+
 type CellConstructor = {
   cellWidth: number
   cellHeight: number
   border: number
   row: number
   column: number
+  type?: CellType
 }
 
 export class Cell extends Graphics {
@@ -13,6 +16,7 @@ export class Cell extends Graphics {
   cellHeight: number
   border: number
   index: [number, number]
+  type?: CellType = 'default'
 
   constructor({cellWidth, cellHeight, border, row, column}: CellConstructor) {
     super()
@@ -21,23 +25,31 @@ export class Cell extends Graphics {
     this.cellHeight = cellHeight
     this.border = border
     this.index = [row, column]
+
+    this.createCell()
+    this.createIndexText()
+  }
+
+  private createCell() {
     this.rect(
       0,
       0,
-      cellWidth,
-      cellHeight
+      this.cellWidth,
+      this.cellHeight
     ).fill({
       color: 0xffffff,
       alpha: 0.5
     }).stroke({
-      width: border,
+      width: this.border,
       color: 0xffea00
     })
-    this.x = cellWidth * column
-    this.y = cellHeight * row
+    this.x = this.cellWidth * this.index[1]
+    this.y = this.cellHeight * this.index[0]
+  }
 
+  private createIndexText() {
     const text = new Text({
-      text: `[${row}, ${column}]`,
+      text: `[${this.index[0]}, ${this.index[1]}]`,
       style: {
         fill: 0xffffff,
         fontSize: 16
@@ -49,5 +61,29 @@ export class Cell extends Graphics {
     text.y = this.height / 2
 
     this.addChild(text)
+  }
+
+  public changeTypeTo(type: CellType) {
+    this.type = type
+
+    switch(type) {
+      case "origin":
+        this.fill({
+          color: 0x00ffff
+        })
+        break
+      case 'destination':
+        this.fill({
+          color: 0x00ff00
+        })
+        break
+      case 'default':
+      default:
+        this.fill({
+          color: 0xffffff,
+          alpha: 0.5
+        })
+        break      
+    }
   }
 }
--- src/grid.ts
+++ src/grid.ts
@@ -5,15 +5,27 @@ export class Grid {
   app: Application
   rows: number = 10
   columns: number = Math.trunc(this.rows * (16/9))
-  cells: Cell[] = []
+  cells: Cell[][] = []
+  originCell: Cell | undefined = undefined
+  destinationCell: Cell | undefined = undefined
 
   constructor(app: Application) {
     this.app = app;
     this.generateGrid()
+    this.defineOriginCell({
+      row: 9,
+      column: 15
+    })
+    this.defineDestinationCell({
+      row: 1,
+      column: 3
+    })
   }
 
   private generateGrid() {
     for (let i = 0; i < this.rows; i++) {
+      this.cells[i] = []
+
       for (let j = 0; j < this.columns; j++) {
         const width = (window.innerWidth / this.columns)
         const height = (window.innerHeight / this.rows)
@@ -25,9 +37,43 @@ export class Grid {
           row: i,
           column: j
         })
-        this.cells.push(cell)
+        this.cells[i].push(cell)
         this.app.stage.addChild(cell)
       }
     }
+  }
+
+  public defineOriginCell({
+    row,
+    column
+  }: {
+    row: number,
+    column: number
+  }) {
+    const currentOriginCell = this.originCell
+    if (currentOriginCell) {
+      currentOriginCell.changeTypeTo('default')
+    }
+
+    const cell = this.cells[row][column]
+    cell.changeTypeTo('origin')
+    this.originCell = cell
+  }
+
+  public defineDestinationCell({
+    row,
+    column
+  }: {
+    row: number,
+    column: number
+  }) {
+    const currentDestinationCell = this.destinationCell
+    if (currentDestinationCell) {
+      currentDestinationCell.changeTypeTo('default')
+    }
+
+    const cell = this.cells[row][column]
+    cell.changeTypeTo('destination')
+    this.destinationCell = cell
   }
 }
